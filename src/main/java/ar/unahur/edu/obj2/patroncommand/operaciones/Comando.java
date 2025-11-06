c81d77785492ce2836772ff55e2898b402579e88
GabrieMonte <igmontenegro17@gmail.com>
2025-11-06 18:50:18 -0300

    terminado

---
+++ src/main/java/ar/unahur/edu/obj2/patroncommand/operaciones/Comando.java
@@ -0,0 +1,22 @@
+package ar.unahur.edu.obj2.patroncommand.operaciones;
+
+import ar.unahur.edu.obj2.patroncommand.microcontrolador.Programable;
+
+public abstract class Comando implements Operable {
+    Programable microBackUp;
+
+    @Override
+    public void execute(Programable micro) {
+        microBackUp = micro.copiar();
+        this.doExecute(micro);
+        micro.incProgramCounter();
+    }
+
+    protected abstract void doExecute(Programable micro);
+    
+    @Override
+    public void undo(Programable micro){
+        micro.copiardesde(microBackUp);
+    }
+
+}
---
+++ src/main/java/ar/unahur/edu/obj2/patroncommand/operaciones/Swap.java
@@ -0,0 +1,15 @@
+package ar.unahur.edu.obj2.patroncommand.operaciones;
+
+import ar.unahur.edu.obj2.patroncommand.microcontrolador.Programable;
+
+public class Swap extends Comando{
+
+    @Override
+    public void doExecute(Programable micro) {
+        Integer valorAuxiliar = micro.getAcumuladorA();
+
+        micro.setAcumuladorA(micro.getAcumuladorB());
+        micro.setAcumuladorB(valorAuxiliar);
+    }
+
+}
